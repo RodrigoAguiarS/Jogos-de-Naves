b74ba4d692a50eba83ad7d1111d24975f579f2d3
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-12-24 19:04:16 -0300

    ultima colisão

--- jogo1/js/js.js
+++ jogo1/js/js.js
@@ -185,6 +185,16 @@ function start() { // Inicio da função start()
             reposicionaAmigo();
             $("#amigo").remove();
         }
+        //Inimigo2 com o amigo
+        if (colisao6.length>0){
+            amigoX = parseInt($("#amigo").css("left"));
+            amigoY = parseInt($("#amigo").css("top"));
+            explosao3(amigoX,amigoY);
+            $("#amigo").remove();
+            
+            reposicionaAmigo();
+        }
+
     }//Fim da função colisao()
 
     //Explosão 1
@@ -264,4 +274,17 @@ function start() { // Inicio da função start()
             }
         }
     }// Fim da função reposicionaAmigo()
+    //Explosão3
+    function explosao3(amigoX,amigoY){
+        $("#fundoGame").append("<div id='explosao3' class='anima4'></div");
+        $("#explosao3").css("top",amigoY);
+        $("#explosao3").css("left",amigoX);
+
+        var tempoExplosao3=window.setInterval(resetaExplosao3, 1000);
+        function resetaExplosao3(){
+            $("#explosao3").remove();
+            window.clearInterval(tempoExplosao3);
+            tempoExplosao3=null;
+        }
+    } // Fim da função explosao3
 } // Fim da função start
