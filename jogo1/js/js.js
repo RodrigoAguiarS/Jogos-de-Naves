19bc0df8e85cd5d38b115dabdd615ecf2a01f7c6
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-12-24 16:04:33 -0300

    Adicinando função de disparo

--- jogo1/js/js.js
+++ jogo1/js/js.js
@@ -8,6 +8,7 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
     //Principais variaveis do jogo
+    var podeAtirar=true;
     var jogo = {};
     var velocidade = 5;
     var posicaoY = parseInt(Math.random() * 334);
@@ -60,7 +61,7 @@ function start() { // Inicio da função start()
             }       
         }
         if (jogo.pressionou[TECLA.D]) {     
-            //Chama função Disparo	
+            disparo();
         }
     } // fim da função movejogador()   
     function moveinimigo1(){
@@ -89,4 +90,32 @@ function start() { // Inicio da função start()
             $("#amigo").css("left",0);  
         }
     }
+    function disparo() {
+        if (podeAtirar==true) {
+            
+        podeAtirar=false;
+        
+        topo = parseInt($("#jogador").css("top"))
+        posicaoX= parseInt($("#jogador").css("left"))
+        tiroX = posicaoX + 190;
+        topoTiro=topo+37;
+        $("#fundoGame").append("<div id='disparo'></div");
+        $("#disparo").css("top",topoTiro);
+        $("#disparo").css("left",tiroX);
+        
+        var tempoDisparo=window.setInterval(executaDisparo, 30);
+        
+        } //Fecha podeAtirar
+            function executaDisparo() {
+            posicaoX = parseInt($("#disparo").css("left"));
+            $("#disparo").css("left",posicaoX+15); 
+                if (posicaoX>900) {
+                            
+                window.clearInterval(tempoDisparo);
+                tempoDisparo=null;
+                $("#disparo").remove();
+                podeAtirar=true;       
+            }
+        } // Fecha executaDisparo()
+    } // Fecha disparo()
 } // Fim da função start
