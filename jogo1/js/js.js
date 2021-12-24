950f9e764c685e815906e22a87c491ebfb0bdb9b
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-12-24 15:02:30 -0300

    limitando as posições de movimentação.

--- jogo1/js/js.js
+++ jogo1/js/js.js
@@ -41,11 +41,18 @@ function start() { // Inicio da função start()
 	
         if (jogo.pressionou[TECLA.W]) {
             var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top",topo-10); 
+            $("#jogador").css("top",topo-10);
+            
+            if (topo<=0) {
+                $("#jogador").css("top",topo+10);
+            } 
         } 
         if (jogo.pressionou[TECLA.S]) {
             var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top",topo+10);	
+            $("#jogador").css("top",topo+10);
+            if (topo>=434) {
+                $("#jogador").css("top",topo-10);
+            }       
         }
         if (jogo.pressionou[TECLA.D]) {     
             //Chama função Disparo	
