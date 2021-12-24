06efb522915130b06f8a2c7a2544d0f562f27f25
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-12-24 16:34:13 -0300

    animação de explosão

--- jogo1/js/js.js
+++ jogo1/js/js.js
@@ -108,8 +108,8 @@ function start() { // Inicio da função start()
         
         } //Fecha podeAtirar
             function executaDisparo() {
-            posicaoX = parseInt($("#disparo").css("left"));
-            $("#disparo").css("left",posicaoX+15); 
+                posicaoX = parseInt($("#disparo").css("left"));
+                $("#disparo").css("left",posicaoX+15); 
                 if (posicaoX>900) {
                             
                 window.clearInterval(tempoDisparo);
@@ -121,6 +121,35 @@ function start() { // Inicio da função start()
     } // Fecha disparo()
     function colisao(){
         var colisao1 = ($("#jogador").collision($("#inimigo1")));
+        // jogador com o inimigo1
+	    if (colisao1.length>0) {
+            
+            inimigo1X = parseInt($("#inimigo1").css("left"));
+            inimigo1Y = parseInt($("#inimigo1").css("top"));
+            explosao1(inimigo1X,inimigo1Y);
+    
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left",694);
+            $("#inimigo1").css("top",posicaoY);
+        }
         console.log(colisao1);
     }//Fim da função colisao()
+    //Explosão 1
+    function explosao1(inimigo1X,inimigo1Y) {
+	    $("#fundoGame").append("<div id='explosao1'></div");
+	    $("#explosao1").css("background-image", "url(imgs/explosao.png)");
+	    var div=$("#explosao1");
+	    div.css("top", inimigo1Y);
+	    div.css("left", inimigo1X);
+	    div.animate({width:200, opacity:0}, "slow");
+	
+	    var tempoExplosao=window.setInterval(removeExplosao, 1000);
+	
+		function removeExplosao() {
+			
+			div.remove();
+			window.clearInterval(tempoExplosao);
+			tempoExplosao=null;
+		}
+	} // Fim da função explosao1()
 } // Fim da função start
