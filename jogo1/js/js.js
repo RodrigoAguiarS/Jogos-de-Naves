23373399f7ec2852afdb7bbb93166dd29182041d
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-12-24 14:22:08 -0300

    adicionado movimento

--- jogo1/js/js.js
+++ jogo1/js/js.js
@@ -7,4 +7,23 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
+    //Principais variaveis do jogo
+
+    var jogo = {};
+    //Game Loop
+
+    jogo.timer = setInterval(loop, 30);
+
+    function loop(){
+        movefundo();
+    }// fim da funcão loop
+    //Função que movimenta o fundo do jogo
+    function movefundo() {
+	
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position",esquerda-1);
+    
+        } // fim da função movefundo()
+
+
 } // Fim da função start
