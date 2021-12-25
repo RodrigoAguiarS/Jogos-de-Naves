8ddb60615ccac21d305a2612216c3a91ddddf5a7
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-12-25 11:04:58 -0300

    aumentando a dificuldade de acordo com os abates

--- jogo1/js/js.js
+++ jogo1/js/js.js
@@ -169,6 +169,7 @@ function start() {
         }
         // Disparo com o inimigo1
         if (colisao3.length>0) {
+            velocidade=velocidade+0.3;
             pontos=pontos+100;
             inimigo1X = parseInt($("#inimigo1").css("left"));
             inimigo1Y = parseInt($("#inimigo1").css("top"));
